53e990278333f59413d8fe5e7c302b5764af970e
uupaa <uupaa.js@gmail.com>
2014-03-18 23:04:24 +0900

    update settings

--- lint/plato/report.history.js
+++ lint/plato/report.history.js
@@ -1,1 +1,46 @@
-__history = [{"date":"Mon, 10 Mar 2014 13:52:49 GMT","total":{"sloc":35,"maintainability":69.17652635949712},"average":{"sloc":35,"maintainability":"69.18"}},{"date":"Mon, 10 Mar 2014 13:53:49 GMT","total":{"sloc":35,"maintainability":69.15757497097216},"average":{"sloc":35,"maintainability":"69.16"}},{"date":"Wed, 12 Mar 2014 18:21:00 GMT","total":{"sloc":35,"maintainability":69.15757497097216},"average":{"sloc":35,"maintainability":"69.16"}}]
+__history = [
+  {
+    "date": "Mon, 10 Mar 2014 13:52:49 GMT",
+    "total": {
+      "sloc": 35,
+      "maintainability": 69.17652635949712
+    },
+    "average": {
+      "sloc": 35,
+      "maintainability": "69.18"
+    }
+  },
+  {
+    "date": "Mon, 10 Mar 2014 13:53:49 GMT",
+    "total": {
+      "sloc": 35,
+      "maintainability": 69.15757497097216
+    },
+    "average": {
+      "sloc": 35,
+      "maintainability": "69.16"
+    }
+  },
+  {
+    "date": "Wed, 12 Mar 2014 18:21:00 GMT",
+    "total": {
+      "sloc": 35,
+      "maintainability": 69.15757497097216
+    },
+    "average": {
+      "sloc": 35,
+      "maintainability": "69.16"
+    }
+  },
+  {
+    "date": "Tue, 18 Mar 2014 14:02:10 GMT",
+    "total": {
+      "sloc": 35,
+      "maintainability": 69.15757497097216
+    },
+    "average": {
+      "sloc": 35,
+      "maintainability": "69.16"
+    }
+  }
+]
