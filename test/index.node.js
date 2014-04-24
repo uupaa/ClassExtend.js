be66e5e1130b108694d5083b7f9325014933c88c
uupaa <uupaa.js@gmail.com>
2014-04-24 22:50:36 +0900

    update setting

--- test/index.node.js
+++ test/index.node.js
@@ -1,6 +1,7 @@
-require("uupaa.task.js");
-require("uupaa.test.js");
+require("../node_modules/uupaa.nodemodule.js/lib/NodeModule.js");
+require("../node_modules/uupaa.valid.js/lib/Valid.js");
+require("../node_modules/uupaa.task.js/lib/Task.js");
+require("../node_modules/uupaa.test.js/lib/Test.js");
 require("../lib/ClassExtend.js");
 require("../release/ClassExtend.min.js");
 require("./test.js");
-
